6369da3f17b469c13303611f74802278acd60a71
terabyte <terabeautyworld@gmail.com>
2026-01-27 22:03:31 +0100

    fix error

--- terraform/main.tf
+++ terraform/main.tf
@@ -20,7 +20,6 @@ resource "null_resource" "bootstrap" {
 
   provisioner "remote-exec" {
     inline = [
-      # ---- user setup ----
       "id platform || useradd -m -s /bin/bash platform",
       "echo 'platform ALL=(ALL) NOPASSWD:ALL' > /etc/sudoers.d/platform",
 
@@ -30,37 +29,24 @@ resource "null_resource" "bootstrap" {
       "chmod 700 /home/platform/.ssh",
       "chmod 600 /home/platform/.ssh/authorized_keys",
 
-      # ---- base packages ----
       "apt update && apt install -y curl git sudo",
 
-      # ---- k3s ----
-      "su - platform -c \"curl -sfL https://get.k3s.io | sh -\"",
+      "su - platform -c 'curl -sfL https://get.k3s.io | sh -'",
 
       "mkdir -p /home/platform/.kube",
       "cp /etc/rancher/k3s/k3s.yaml /home/platform/.kube/config",
       "chown -R platform:platform /home/platform/.kube",
 
-      # ---- helm ----
-      "su - platform -c \"curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash\"",
+      "su - platform -c 'curl https://raw.githubusercontent.com/helm/helm/main/scripts/get-helm-3 | bash'",
 
-      # ---- argo cd ----
-      "su - platform -c \"kubectl create namespace argocd || true\"",
-      "su - platform -c \"kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml\"",
+      "su - platform -c 'kubectl create namespace argocd || true'",
+      "su - platform -c 'kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml'",
 
-      # ---- ghcr image pull secret ----
-      "su - platform -c \"kubectl create secret docker-registry ghcr-secret \
-        --docker-server=ghcr.io \
-        --docker-username=${var.ghcr_username} \
-        --docker-password=${var.ghcr_token} \
-        --dry-run=client -o yaml | kubectl apply -f -\"",
+      "su - platform -c 'kubectl create secret docker-registry ghcr-secret --docker-server=ghcr.io --docker-username=${var.ghcr_username} --docker-password=${var.ghcr_token} --dry-run=client -o yaml | kubectl apply -f -'",
 
-      # ---- monitoring ----
-      "su - platform -c \"helm repo add prometheus-community https://prometheus-community.github.io/helm-charts\"",
-      "su - platform -c \"helm repo update\"",
-      "su - platform -c \"helm install monitoring prometheus-community/kube-prometheus-stack \
-        --namespace monitoring --create-namespace \
-        --set prometheus.prometheusSpec.retention=3d \
-        --set grafana.resources.requests.memory=150Mi\""
+      "su - platform -c 'helm repo add prometheus-community https://prometheus-community.github.io/helm-charts'",
+      "su - platform -c 'helm repo update'",
+      "su - platform -c 'helm install monitoring prometheus-community/kube-prometheus-stack --namespace monitoring --create-namespace --set prometheus.prometheusSpec.retention=3d --set grafana.resources.requests.memory=150Mi'"
     ]
   }
 }
